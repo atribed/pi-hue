624ae243714eaa4896ec80b84e44d3d4ca48beb8
Arthur Gorka <azgorka@gmail.com>
2016-08-14 22:10:44 -0400

    add some JS to change color temp and pictures for pi

--- pi-hue.py
+++ pi-hue.py
@@ -5,13 +5,30 @@
 
 """
 
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, jsonify
+import picamera
 
-app = Flask(__name__)
+# Not ideal, but since it's just being used across a home network not concerned at the moment.
+app = Flask(__name__, static_url_path='/static')
+
+# Setting up camera so we can submit pictures to set a scene with Hue.
+camera = picamera.PiCamera()
 
 @app.route('/')
 def load_home():
     return render_template('index.html')
 
+@app.route('/picture', methods=['POST'])
+def take_picture():
+    images_path = 'static/images/image.jpg'
+    camera.capture(images_path)
+    #
+    # Just a temp response for the time being.
+    return jsonify(
+        success=True,
+        imagePath=images_path
+    )
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    app.run()
--- static/js/pie-hue.js
+++ static/js/pie-hue.js
@@ -1,4 +1,81 @@
-// Testing that everything works.
+// Config settings
+
 (function() {
-    window.console.log('Pi-Hue has loaded JS successfully!');
+    var apiUsername = 'exampleusername'; // This is super gross. Will move it to server and proxy once things are working.
+    var bridgeAddress = 'http://0.0.0.0'; // path to bridge (should be IP)
+
+    // Dirty take picture request, will clean.
+    function requestPicture() {
+        $.ajax({
+            url: '/picture',
+            method: 'POST',
+            data: {
+                picture: true
+            }
+        });
+    }
+
+    // Gets group info from server.
+    function getGroupInfo() {
+        var groupRequest = $.ajax({
+            url: bridgeAddress + '/api/' + apiUsername + '/groups/'
+        });
+
+        return groupRequest;
+    }
+
+    // Sends a request to get groups and finds the ID for the one you want.
+    function getGroupIdByName(groupName, callback) {
+        var groupInfoRequest = getGroupInfo();
+
+        groupInfoRequest.done(function(data) {
+            var groupId = '';
+            _.each(data, function(group, id) {
+                if (group.name === groupName) {
+                    groupId = id;
+
+                    return;
+                }
+            });
+
+            callback(groupId);
+        });
+    }
+
+    // Sets group settings by a group name.
+    function setGroupSettingsByName(name, setting, value) {
+        var data = {};
+        data[setting] = value;
+
+        var callback = function(id) {
+            $.ajax({
+                url: bridgeAddress + '/api/' + apiUsername + '/groups/' + id + '/action',
+                data: window.JSON.stringify(data),
+                type: 'PUT'
+            });
+        };
+
+        getGroupIdByName(name, callback);
+    }
+
+    // Quick and dirty, will refactor after sleep.
+    function setColorTemp(event) {
+        event.preventDefault();
+        $target = $(event.target);
+
+        var value = $target.find('[name="ct"]').val();
+        var room = $target.find('[name="room"]').val();
+
+        setGroupSettingsByName(room, 'ct', parseInt(value, 10));
+    };
+
+    $(document).ready(function() {
+        window.console.log('Pi-Hue has loaded JS successfully!');
+
+        // Takes a picture with the pi.
+        $('#takePicture').click(requestPicture);
+
+        // Changes color temp.
+        $('#setCt').submit(setColorTemp);
+    });
 })();
